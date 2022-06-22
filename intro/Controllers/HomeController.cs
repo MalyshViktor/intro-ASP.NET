739c3322e11e6294409ba976ba7b0eaa78961fcb
MalyshViktor <viktor.motodom@gmail.com>
2022-06-22 22:16:14 +0300

    Добавьте файлы проекта.

---
+++ intro/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using intro.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            var model = new AboutModel
+            {
+                Data = "The Model Data"
+            };
+            return View(model);
+        }
+
+        public IActionResult Contacts()
+        {
+            var contacts = new ContactsModel
+            {
+                Address = "Pushkinskaya str, 19",
+                PhoneNumber = "322-223",
+                Name = "Rozetka LLC"
+            };
+            return View(contacts);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ intro/Models/ContactsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace intro.Models
+{
+    public class ContactsModel
+    {
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
